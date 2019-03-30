a5bde2e7cad26de726ea74a95d7aa6b4ca69f68e
Sunny Nagam <sunnynagam1@gmail.com>
2019-03-30 17:58:20 -0600

    yolo

--- backend/trainer.py
+++ backend/trainer.py
@@ -10,13 +10,14 @@ from keras.models import Sequential
 from keras.layers import Dense, LeakyReLU
 from keras.wrappers.scikit_learn import KerasRegressor
 
-def strech(num):
+def stretch(num):
 	return num * (maxY-minY) +minY
 # load dataset
-dataframe = pandas.read_csv("datafiles/merged.csv", header=0)
+dataframe = pandas.read_csv("datafiles/statscanada/newMH.csv", header=1)
 dataset = dataframe.values
+"Perceived mental health, fair or poor","Perceived mental health, very good or excellent",
 # split into input (X) and output (Y) variables
-X = dataset[:,0:-1]
+X = dataset[:,1:-1]
 Y = dataset[:,-1]
 #X = np.reshape(X, (-1,1))
 Y = np.reshape(Y, (-1,1))
@@ -27,16 +28,20 @@ maxY = np.max(Y)
 scaler = MinMaxScaler()
 scaler.fit(X)
 Xnew=scaler.transform(X)
+# Xnew = X
 scaler.fit(Y)
 Ynew=scaler.transform(Y)
+# Ynew = Y
 
 print(Xnew)
 print(Ynew)
-opt = keras.optimizers.Adam(lr=0.0001)
+opt = keras.optimizers.Adam(lr=0.001)
 
 model = Sequential()
 model.add(Dense(3, input_dim=3, kernel_initializer='normal', activation='relu'))
-model.add(Dense(1, kernel_initializer='normal', activation='relu'))
+model.add(Dense(10, kernel_initializer='normal', activation=LeakyReLU()))
+model.add(Dense(5, kernel_initializer='normal', activation=LeakyReLU()))
+model.add(Dense(1, kernel_initializer='normal', activation='sigmoid'))
 model.compile(loss='mean_squared_error', optimizer=opt,  metrics=['accuracy'])
 model.summary()
 
@@ -44,6 +49,6 @@ history = model.fit(Xnew, Ynew, epochs=100, verbose=2, shuffle=True)
 
 for i in range(0,10):
 	results = model.predict(Xnew[i:i+1])
-	print(Xnew[i:i+1],"->",strech(results[0,0]))
+	print(Xnew[i:i+1],"->",stretch(results[0,0]), Ynew[i:i+1])
 
 
