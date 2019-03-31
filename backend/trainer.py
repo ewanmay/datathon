7d1f1c8019ffea80ffb06bb265d040bcd4794eb7
Sunny Nagam <sunnynagam1@gmail.com>
2019-03-30 21:51:58 -0600

    nothin works

--- backend/trainer.py
+++ backend/trainer.py
@@ -10,10 +10,34 @@ from sklearn.preprocessing import MinMaxScaler
 from keras.models import Sequential
 from keras.layers import Dense, LeakyReLU, Dropout
 from keras.wrappers.scikit_learn import KerasRegressor
+import keras.backend as K
 from core import ref
 
 def stretch(num):
 	return num * (maxY-minY) +minY
+
+def reverse(inputArr):
+	loss = 1 - K.mean(model.output)
+	grads = K.gradients(loss, inputArr)
+	grads /= (K.sqrt(K.mean(K.square(grads)))+ 1e-5) #yikes
+
+	iterate = K.function([inputArr],[loss,grads])
+
+	step = 0.1
+
+	inputArrNew = inputArr.copy()
+
+	for i in range(100):
+	     loss_val, grads_val = iterate([inputArrNew])
+	     inputArrNew -= grads_val[0] * step
+	     print("iter:",i,np.mean(loss_value))
+
+	print("Before:")
+	print(model.predict(inputArr))
+
+
+	print("After:")
+	print(model.predict(inputArrNew))
 # load dataset
 dataframe = pandas.read_csv("datafiles/statscanada/newMH_percents.csv", header=0)
 dataset = dataframe.values
@@ -38,7 +62,7 @@ Xtrain, Xtest, Ytrain, Ytest = train_test_split(Xnew, Ynew, test_size=0.2)
 print(Xnew)
 print(Y)
 
-opt = keras.optimizers.Adam(lr=0.0001)
+opt = keras.optimizers.Adam(lr=0.00003)
 
 model = Sequential()
 model.add(Dense(13, input_dim=13, kernel_initializer='normal', activation='relu'))
@@ -49,7 +73,7 @@ model.add(Dense(1, kernel_initializer='normal', activation='sigmoid'))
 model.compile(loss='mean_squared_error', optimizer=opt,  metrics=['accuracy'])
 model.summary()
 
-history = model.fit(Xtrain, Ytrain, epochs=500, validation_data=(Xtest,Ytest), verbose=2, shuffle=True)
+history = model.fit(Xtrain, Ytrain, epochs=200, validation_data=(Xtest,Ytest), verbose=2, shuffle=True)
 # "Loss"
 plt.plot(history.history['loss'])
 plt.plot(history.history['val_loss'])
@@ -67,12 +91,16 @@ scores = model.evaluate(Xtest, Ytest, verbose=1)
 print(scores)
 
 newPatient = Xtest[2:3]
+print("-------------")
+print(K.mean(model.predict(newPatient)))
+print("-------------")
 print(newPatient)
 print(stretch(model.predict(newPatient)[0,0]))
 
+reverse(newPatient)
+
 newPatient[0,ref["Fruit and vegetable consumption, 5 times or more per day"]] -= 0.3
 newPatient[0,ref["Perceived life stress, most days quite a bit or extremely stressful"]] += 0.3
-
 print(newPatient)
 print(stretch(model.predict(newPatient)[0,0]))
 
