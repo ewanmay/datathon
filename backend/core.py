33de70657a26e36fde7d8fd3d24b7a7a5d16bc88
Sunny Nagam <sunnynagam1@gmail.com>
2019-03-30 21:02:11 -0600

    added ref

--- backend/core.py
+++ backend/core.py
@@ -13,6 +13,22 @@ import numpy as np
 
 from keras.backend.tensorflow_backend import set_session
 
+ref = {
+	"Body mass index, adjusted self-reported, adult (18 years and over), obese":0,
+	"Body mass index, adjusted self-reported, adult (18 years and over), overweight":1,
+	"Body mass index, self-reported, youth (12 to 17 years old), overweight or obese":2,
+	"Current smoker, daily or occasional":3,
+	"Fruit and vegetable consumption, 5 times or more per day":4,
+	"High blood pressure":5,
+	"Life satisfaction, satisfied or very satisfied":6,
+	"Mood disorder":7,
+	"Perceived health, very good or excellent":8,
+	"Perceived life stress, most days quite a bit or extremely stressful":9,
+	"Self-reported physical activity, 150 minutes per week, adult (18 years and over)":10,
+	"Self-reported physical activity, average 60 minutes per day, youth (12 to 17 years old)":11,
+	"Sense of belonging to local community, somewhat strong or very strong":12
+}
+
 config = tf.ConfigProto(
     gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=0.8)
     # device_count = {'GPU': 1}
--- backend/trainer.py
+++ backend/trainer.py
@@ -8,28 +8,14 @@ from sklearn.model_selection import KFold
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import MinMaxScaler
 from keras.models import Sequential
-from keras.layers import Dense, LeakyReLU
+from keras.layers import Dense, LeakyReLU, Dropout
 from keras.wrappers.scikit_learn import KerasRegressor
-
-ref = {
-	"Body mass index, adjusted self-reported, adult (18 years and over), obese":0,
-	"Body mass index, adjusted self-reported, adult (18 years and over), overweight":1,
-	"Body mass index, self-reported, youth (12 to 17 years old), overweight or obese":2,
-	"Current smoker, daily or occasional":3,
-	"Fruit and vegetable consumption, 5 times or more per day":4,
-	"Life satisfaction, satisfied or very satisfied":5,
-	"Mood disorder":6,
-	"Perceived health, very good or excellent":7,
-	"Perceived life stress, most days quite a bit or extremely stressful":8,
-	"Self-reported physical activity, 150 minutes per week, adult (18 years and over)":9,
-	"Self-reported physical activity, average 60 minutes per day, youth (12 to 17 years old)":10,
-	"Sense of belonging to local community, somewhat strong or very strong":11
-}
+from core import ref
 
 def stretch(num):
 	return num * (maxY-minY) +minY
 # load dataset
-dataframe = pandas.read_csv("datafiles/statscanada/newMH.csv", header=0)
+dataframe = pandas.read_csv("datafiles/statscanada/newMH_percents.csv", header=0)
 dataset = dataframe.values
 # split into input (X) and output (Y) variables
 X = np.concatenate((dataset[:,5:-4], dataset[:,-3:]), axis = 1)
@@ -51,11 +37,11 @@ Xtrain, Xtest, Ytrain, Ytest = train_test_split(Xnew, Ynew, test_size=0.2)
 
 print(Xnew)
 print(Y)
-print([stretch(num) for num in Ynew])
+
 opt = keras.optimizers.Adam(lr=0.0001)
 
 model = Sequential()
-model.add(Dense(12, input_dim=12, kernel_initializer='normal', activation='relu'))
+model.add(Dense(13, input_dim=13, kernel_initializer='normal', activation='relu'))
 model.add(Dense(100, kernel_initializer='normal', activation=LeakyReLU()))
 model.add(Dense(50, kernel_initializer='normal', activation=LeakyReLU()))
 model.add(Dense(25, kernel_initializer='normal', activation=LeakyReLU()))
@@ -63,7 +49,7 @@ model.add(Dense(1, kernel_initializer='normal', activation='sigmoid'))
 model.compile(loss='mean_squared_error', optimizer=opt,  metrics=['accuracy'])
 model.summary()
 
-history = model.fit(Xtrain, Ytrain, epochs=300, validation_data=(Xtest,Ytest), verbose=2, shuffle=True)
+history = model.fit(Xtrain, Ytrain, epochs=500, validation_data=(Xtest,Ytest), verbose=2, shuffle=True)
 # "Loss"
 plt.plot(history.history['loss'])
 plt.plot(history.history['val_loss'])
@@ -75,7 +61,7 @@ plt.show()
 
 for i in range(0,len(Xtest)):
 	results = model.predict(Xtest[i:i+1])
-	print(Xtest[i:i+1],"->",stretch(results[0,0]), stretch(Ytest[i:i+1]))
+	print(Xtest[i:i+1],"->",(results[0,0]), (Ytest[i:i+1]))
 
 scores = model.evaluate(Xtest, Ytest, verbose=1)
 print(scores)
